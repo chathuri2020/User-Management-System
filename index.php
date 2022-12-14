046114c25f2e789393491e0dd52c8b920d87b3df
chathuri2020 <71623402+chathuri2020@users.noreply.github.com>
2022-12-15 01:16:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>User Management System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/index.css">
+
+</head>
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <aside>
+  <p> <img src="Images/dark-logo.png" alt="" width="100%" height="10%" style="padding: 10%;"></p>
+
+  <a href="User_Registration.php/">
+    <i class="fa fa-user-o" aria-hidden="true"></i>
+    Dashboard
+  </a>
+  <a href="javascript:void(1)">
+    <i class="fa fa-laptop" aria-hidden="true"></i>
+    Add users
+  </a>
+  <a href="javascript:void('User_Registration.php')">
+    <i class="fa fa-clone" aria-hidden="true"></i>
+    Manage users
+  </a>
+  
+</aside>
+
+<div class="social">
+  <a href="https://www.linkedin.com/in/florin-cornea-b5118057/" target="_blank">
+    <i class="fa fa-linkedin"></i>
+  </a>
+</div>
+
+
+
+
+
+
+</body>
+</html>
